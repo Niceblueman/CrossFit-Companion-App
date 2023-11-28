2c6886e42f9cf5c5f8cb058611e56c6697db5d6d
Niceblueman <wintheiser761@gmail.com>
2023-11-29 00:04:19 +0100

    all

--- src/context/Preferences.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-const PreferencesContext = createContext({
-  theme: '',
-  toggleTheme: () => {},
-});
-
-export default PreferencesContext;
--- src/hooks/usePreferences.js
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import Preferences from '../context/Preferences';
-
-export default () => useContext(Preferences);
